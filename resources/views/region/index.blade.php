9db3ef94d145482a7e3b7c116005ec7c9dc7362f
inettman <inettman@gmail.com>
2016-02-10 23:38:21 +0200

    remove cities from main page

--- resources/views/region/index.blade.php
+++ resources/views/region/index.blade.php
@@ -11,15 +11,6 @@
         @foreach ($regions as $region)
             <li class="list-group-item">
                 <h4><a href="{{ route('region', array('id'=>$region->id)) }}">{{ $region->name }}</a></h4>
-                @if (count($region->childred))
-                    <ul>
-                        @foreach ($region->childred as $child)
-                            <li>
-                                <small><a href="{{ route('region', array('id'=>$child->id)) }}">{{ $child->name }}</a></small>
-                            </li>    
-                        @endforeach
-                    </ul>
-                @endif   
             </li>
         @endforeach
     </ul>
